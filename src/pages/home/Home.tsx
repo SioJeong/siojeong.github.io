a54155bf86077f64b49c3754687c2933f544d66b
jeongsangyoung <jeongsangyoung@naver.com>
2024-09-16 23:58:19 +0900

    feat: 애플 뮤직 플레이리스트 임베드

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -21,12 +21,16 @@ export default function Home() {
             try {
                 // 네트워크 요청 병렬 처리
                 const markdownResponse = fetch(`/markdowns/home/intro.md`);
-                const markdownPaths = Array.from({ length: RECENT_POSTS_STANDARD }, (_, i) => 
-                    `/markdowns/posts/${totalPostsNumber - i}.md`
+                const markdownPaths = Array.from(
+                    { length: RECENT_POSTS_STANDARD },
+                    (_, i) => `/markdowns/posts/${totalPostsNumber - i}.md`
                 );
 
                 const titlesPromise = fetchRecentPostsTitles(markdownPaths);
-                const [markdownResult, titles] = await Promise.all([markdownResponse, titlesPromise]);
+                const [markdownResult, titles] = await Promise.all([
+                    markdownResponse,
+                    titlesPromise,
+                ]);
 
                 if (!markdownResult.ok) throw new Error('Failed to fetch markdown file');
                 const text = await markdownResult.text();
@@ -43,6 +47,19 @@ export default function Home() {
     return (
         <div>
             <img src="/Symbol.svg" alt="Symbol" className={styles.symbol} />
+            <iframe
+                allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+                frameBorder="0"
+                height="450"
+                style={{
+                    width: '100%',
+                    maxWidth: '660px',
+                    overflow: 'hidden',
+                    borderRadius: '10px',
+                }}
+                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+                src="https://embed.music.apple.com/kr/playlist/24%EB%85%84-9%EC%9B%94/pl.u-AkAmPNyt27LV71x"
+            />
             <MarkdownRenderer markdown={markdown} />
             <h1 className={styles.recentPostsTitle}>Recently Posted</h1>
             <ul className={styles.recentPostsList}>
