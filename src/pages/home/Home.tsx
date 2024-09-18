602a1e73088f7898da65dcad030c81ec1d4c7d68
jeongsangyoung <jeongsangyoung@naver.com>
2024-09-18 15:03:53 +0900

    feat: playlist 링크 추가 및 홈 화면 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import MarkdownRenderer from '../../components/markdown-renderer/MarkdownRenderer';
 import usePostContext from '../../context/PostContext';
 import fetchRecentPostsTitles from '../../utils/FetchRecentPostsInfos.ts';
 import styles from './Home.module.css';
@@ -13,28 +12,17 @@ interface PostTitle {
 
 export default function Home() {
     const { totalPostsNumber } = usePostContext();
-    const [markdown, setMarkdown] = useState<string>('');
     const [recentPostsTitles, setRecentPostsTitles] = useState<PostTitle[]>([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                // 네트워크 요청 병렬 처리
-                const markdownResponse = fetch(`/markdowns/home/intro.md`);
                 const markdownPaths = Array.from(
                     { length: RECENT_POSTS_STANDARD },
                     (_, i) => `/markdowns/posts/${totalPostsNumber - i}.md`
                 );
 
-                const titlesPromise = fetchRecentPostsTitles(markdownPaths);
-                const [markdownResult, titles] = await Promise.all([
-                    markdownResponse,
-                    titlesPromise,
-                ]);
-
-                if (!markdownResult.ok) throw new Error('Failed to fetch markdown file');
-                const text = await markdownResult.text();
-                setMarkdown(text);
+                const titles = await fetchRecentPostsTitles(markdownPaths);
                 setRecentPostsTitles(titles.slice(0, RECENT_POSTS_STANDARD));
             } catch (error) {
                 console.error(error);
@@ -45,22 +33,15 @@ export default function Home() {
     }, [totalPostsNumber]);
 
     return (
-        <div>
-            <img src="/Symbol.svg" alt="Symbol" className={styles.symbol} />
-            <iframe
-                allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
-                frameBorder="0"
-                height="450"
-                style={{
-                    width: '100%',
-                    maxWidth: '660px',
-                    overflow: 'hidden',
-                    borderRadius: '10px',
-                }}
-                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-                src="https://embed.music.apple.com/kr/playlist/24%EB%85%84-9%EC%9B%94/pl.u-AkAmPNyt27LV71x"
-            />
-            <MarkdownRenderer markdown={markdown} />
+        <section>
+            <div className={styles.audioContainer}>
+                <a
+                    href="https://music.apple.com/kr/playlist/24%EB%85%84-9%EC%9B%94/pl.u-AkAmPNyt27LV71x"
+                    target="_blank"
+                >
+                    <img src="/audio.jpg" className={styles.audio} alt="audio" />
+                </a>
+            </div>
             <h1 className={styles.recentPostsTitle}>Recently Posted</h1>
             <ul className={styles.recentPostsList}>
                 {recentPostsTitles.map(({ title }, index) => (
@@ -69,6 +50,6 @@ export default function Home() {
                     </li>
                 ))}
             </ul>
-        </div>
+        </section>
     );
 }
