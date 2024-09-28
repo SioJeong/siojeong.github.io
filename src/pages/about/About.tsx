508f6cc5d0b6744afa292335cca68a8e9b5a8766
jeongsangyoung <jeongsangyoung@naver.com>
2024-09-28 11:46:00 +0900

    desigh: ui 변경

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -10,8 +10,10 @@ export default function About() {
             </p>
             <p>
                 패션을 전공한 후, 2년간 패션 MD로 일했습니다. 그러나{' '}
-                <strong>더 나은 것을 만드는 과정</strong>에 깊은 매력을 느껴, 개발자로서 새로운
-                커리어를 시작했습니다.
+                <a href="https://siojeong.github.io/post/7" className={styles.linkedpost}>
+                    <strong>더 나은 것을 만드는 것</strong>에 매력을 느껴, 개발자로서 새로운
+                    커리어를 시작했습니다. <img src="/nav-icons/link.svg" alt="linkedpost" />
+                </a>
             </p>
             <p>
                 <strong>고민하는 것</strong>을 좋아합니다. 이러한 생각들을 기록하고 나누기 위해
