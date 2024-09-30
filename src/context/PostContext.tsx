fe9c05467fe53159cd427741ac3aff9289f4742c
정상영 <jeongsangyoung@naver.com>
2024-10-01 01:46:29 +0900

    docs: 게시글 추가

--- src/context/PostContext.tsx
+++ src/context/PostContext.tsx
@@ -10,7 +10,7 @@ const PostContext = createContext<
 
 // Provider 컴포넌트
 export function PostProvider({ children }: { children: ReactNode }) {
-    const [totalPostsNumber, setTotalPostsNumber] = useState(8);
+    const [totalPostsNumber, setTotalPostsNumber] = useState(9);
 
     return (
         <PostContext.Provider value={{ totalPostsNumber, setTotalPostsNumber }}>
